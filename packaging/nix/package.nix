b9f1092b5b52973fa36880a16235f8c48b6e38f2
Kushagra Kumar <kkushagra86@gmail.com>
2026-09-21 07:54:48 +0530

    docs(readme): sync all installation commands and packaging manifests to v0.16.0

--- packaging/nix/package.nix
+++ packaging/nix/package.nix
@@ -7,7 +7,7 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "kkfetch";
-  version = "0.15.2";
+  version = "0.16.0";
 
   src = if src != null then src else fetchFromGitHub {
     owner = "kk376";
